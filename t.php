173a3bcf45a84b4e8676efe5a645c916f5cb107d
Devesh <deveshbaruah@gmail.com>
2018-12-21 01:22:31 +0530

    Assamese Typing Cods

---
+++ t.php
@@ -0,0 +1,180 @@
+<html>
+<Title>Online Text Editor :: Nazira.in</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" href="/image/main/icon.gif" type="image/gif" sizes="16x16">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<script>
+$(document).ready(function(){
+  $("#red").click(function(){
+    $("div").addClass("w3-yellow");
+    $("input").addClass("w3-button w3-red w3-round");
+  });
+});
+</script>
+    <input type="button" id="red" value="Colour Red"  class="w3-button w3-red w3-round"/>
+<h1 class="w3-green w3-padding">Arizan Assamese Text Editor <input type="button" id="pp" value="Hide"  class="w3-button w3-red w3-round"/><input type="button" id="ss" value="Show"  class="w3-button w3-red w3-round"/></h1>
+<div class="w3-container">
+
+<div class="w3-container">
+<form name="cal" method="post" action="" id="textt">
+<textarea name="box" class="w3-input w3-border" style="height:30%;"></textarea><br>
+<center>
+	<input type="button" value=" ১ " onclick="cal.box.value +='১'" class="w3-green w3-button  w3-round">
+		<input type="button" value=" ২ " onclick="cal.box.value +='২'" class="w3-button w3-green w3-round">
+			<input type="button" value=" ৩ " onclick="cal.box.value +='৩" class="w3-button w3-green w3-round">
+				<input type="button" value=" ৪ " onclick="cal.box.value +='৪'" class="w3-button w3-green w3-round">
+					<input type="button" value=" ৫ " onclick="cal.box.value +='৫'" class="w3-button w3-green w3-round">
+						<input type="button" value=" ৬ " onclick="cal.box.value +='৬'" class="w3-button w3-green w3-round">
+							<input type="button" value=" ৭ " onclick="cal.box.value +='৭'" class="w3-button w3-green w3-round">
+								<input type="button" value=" ৮ " onclick="cal.box.value +='৮'" class="w3-button w3-green w3-round">
+									<input type="button" value=" ৯ " onclick="cal.box.value +='৯'" class="w3-button w3-green w3-round">
+										<input type="button" value=" ০ " onclick="cal.box.value +='০'" class="w3-button w3-green w3-round">
+											<input type="button" value=" + " onclick="cal.box.value +='+'" class="w3-button w3-green w3-round">
+												<input type="button" value=" - " onclick="cal.box.value +='-'" class="w3-button w3-green w3-round">
+										<input type="button" value=" = " onclick="cal.box.value +='='" class="w3-button w3-green w3-round">
+										<input type="button" value=" ? " onclick="cal.box.value +='?'" class="w3-button w3-green w3-round">
+	<input type="button" value=" া " onclick="cal.box.value += 'া'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ি " onclick="cal.box.value += 'ি'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ী " onclick="cal.box.value += 'ী'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ু " onclick="cal.box.value += 'ু'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ূ " onclick="cal.box.value += 'ূ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ৃ " onclick="cal.box.value += 'ৃ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ে " onclick="cal.box.value += 'ে'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ৈ " onclick="cal.box.value += 'ৈ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ো " onclick="cal.box.value += 'ো'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ৌ " onclick="cal.box.value += 'ৌ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" অ " onclick="cal.box.value +='অ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" আ " onclick="cal.box.value += 'আ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ই " onclick="cal.box.value += 'ই'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ঈ " onclick="cal.box.value += 'ঈ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" উ " onclick="cal.box.value += 'উ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ঊ " onclick="cal.box.value += 'ঊ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ঋ " onclick="cal.box.value += 'ঋ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" এ " onclick="cal.box.value += 'এ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ঐ " onclick="cal.box.value += 'ঐ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ও " onclick="cal.box.value += 'ও'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ঔ " onclick="cal.box.value += 'ঔ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ক " onclick="cal.box.value += 'ক'" class="w3-button w3-green w3-round">
+	<input type="button" value=" খ " onclick="cal.box.value += 'খ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" গ " onclick="cal.box.value += 'গ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ঘ " onclick="cal.box.value += 'ঘ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ঙ " onclick="cal.box.value += 'ঙ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" চ " onclick="cal.box.value += 'চ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ছ " onclick="cal.box.value += 'ছ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" জ " onclick="cal.box.value += 'জ'" class="w3-button w3-green w3-round">
+    <input type="button" value=" ঝ " onclick="cal.box.value += 'ঝ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ঞ " onclick="cal.box.value += 'ঞ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ট " onclick="cal.box.value += 'ট'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ঠ " onclick="cal.box.value += 'ঠ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ড " onclick="cal.box.value += 'ড'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ঢ " onclick="cal.box.value += 'ঢ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ণ " onclick="cal.box.value += 'ণ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ত " onclick="cal.box.value += 'ত'" class="w3-button w3-green w3-round">
+	<input type="button" value=" থ " onclick="cal.box.value += 'থ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" দ " onclick="cal.box.value += 'দ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ধ " onclick="cal.box.value += 'ধ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ন " onclick="cal.box.value += 'ন'" class="w3-button w3-green w3-round">
+	<input type="button" value=" প " onclick="cal.box.value += 'প'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ফ " onclick="cal.box.value += 'ফ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ব " onclick="cal.box.value += 'ব'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ভ " onclick="cal.box.value += 'ভ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ম " onclick="cal.box.value += 'ম'" class="w3-button w3-green w3-round">
+	<input type="button" value=" য " onclick="cal.box.value += 'য'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ৰ " onclick="cal.box.value += 'ৰ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ল " onclick="cal.box.value += 'ল'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ৱ " onclick="cal.box.value += 'ৱ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" শ " onclick="cal.box.value += 'শ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ষ " onclick="cal.box.value += 'ষ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" স " onclick="cal.box.value += 'স'" class="w3-button w3-green w3-round">
+	<input type="button" value=" হ " onclick="cal.box.value += 'হ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ক্ষ "   onclick="cal.box.value += 'ক্ষ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ড় "  onclick="cal.box.value += 'ড়'"  class="w3-button w3-green w3-round">
+	<input type="button" value=" ঢ় " onclick="cal.box.value += 'ঢ়'" class="w3-button w3-green w3-round">
+	<input type="button" value=" য় " onclick="cal.box.value += 'য়'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ৎ " onclick="cal.box.value += 'ৎ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ং " onclick="cal.box.value += 'ং" class="w3-button w3-green w3-round">
+		<input type="button" value=" ঃ " onclick="cal.box.value += 'ঃ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ঁ " onclick="cal.box.value += 'ঁ'" class="w3-button w3-green w3-round">
+	<input type="button" value=" ং " onclick="cal.box.value += 'ং'" class="w3-button w3-green w3-round">
+	<input type="button" value="Space" onclick="cal.box.value += '&nbsp;'" class="w3-button w3-green w3-round">
+	<input type="button" value="Enter" onclick="cal.box.value += '\n'" class="w3-button w3-green w3-round">
+	<input type="button" value="Clear" onclick=TextC() class="w3-button w3-green w3-round">
+	<input type="button" value="Joiner ্ " onclick="cal.box.value += '্'" class="w3-button w3-green w3-round"><br>
+<input type="text" name="filename" value="" placeholder="File Name">
+<input type="submit" name="submitsave" value="Save" class="w3-button w3-green w3-round">
+	</form>
+<form name="openfile" method="post" action="">
+        <input type="text" name="filename" value="" placeholder="File Open">
+        <input type="submit" name="submitopen" value="Open" class="w3-button w3-green w3-round">
+</form>
+	 <?php
+    if (isset($_POST)){
+        if ($_POST['submitsave'] == "Save"  && !empty($_POST['filename'])) {
+            if(!file_exists($_POST['filename'] . ".txt")){
+                $file = tmpfile();
+            }
+            $file = fopen($_POST['filename'] . ".txt","a+");
+            while(!feof($file)){
+                $old = $old . fgets($file). "<br />";
+            }
+            $text = $_POST["box"];
+            file_put_contents($_POST['filename'] . ".txt", $old . $text);
+            fclose($file);
+        }
+
+        if ($_POST['submitopen'] == "Open") {
+            if(!file_exists($_POST['filename'] . ".txt")){
+                exit("Error: File does not exist.");
+            }
+            $file = fopen($_POST['filename'] . ".txt", "r");
+            while(!feof($file)){
+                echo fgets($file). "<br/>";
+            }
+            fclose($file);
+        }
+    }
+    ?>
+<script>
+
+$(document).ready(function(){
+    $("#pp").click(function(){
+        $("#textt").hide();
+    });
+    $("#ss").click(function(){
+        $("#textt").show();
+    });
+});
+
+function Topen() {
+    alert("Are You Save This Text?");
+
+}
+function TextC() {
+    alert("Are You Clear This Text?");
+    
+}
+function Exit() 
+{
+    alert("Are You Exit?");
+    window.open('index.php','_self');
+}
+</script>
+<input type="button"  value="Exit" onclick="Exit()" class="w3-button w3-red w3-round"/> <br><br>
+<script>
+document.querySelector("button").onclick=function(){window.print();}
+</script>
+<script>
+document.addEventListener("keydown", function(event) {
+  console.log(event.which);
+}
+</script>
+</center>
+</div>
+
+<div class="w3-green w3-center w3-padding-24"><a href="https://Www.Nazira.in"><h3>Www.Nazira.in</h3></a>
+   <h4>Powered by <a href="https://www.arizaninstitute.com" target="_blank">Arizan</a></h4></div>
+</html>
+
+
